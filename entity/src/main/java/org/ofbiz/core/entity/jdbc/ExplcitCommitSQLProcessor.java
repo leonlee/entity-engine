13b5b8c7552846a2a2538cd58a8cdf3831641fef
Andrew Swan <aswan@atlassian.com>
2013-09-24 09:27:23 +1000

    JDEV-24820 Deprecated class with typo in name and added correctly spelled class

--- entity/src/main/java/org/ofbiz/core/entity/jdbc/ExplcitCommitSQLProcessor.java
+++ entity/src/main/java/org/ofbiz/core/entity/jdbc/ExplcitCommitSQLProcessor.java
@@ -7,8 +7,10 @@ package org.ofbiz.core.entity.jdbc;
  * It will be created with {@link org.ofbiz.core.entity.jdbc.SQLProcessor.CommitMode#EXPLICIT_COMMIT}
  *
  * This class is used to "clearly document" the mode the SQLProcessor is in
-
+ *
+ * @deprecated use ExplicitCommitSQLProcessor instead (fixes typo in name)
  */
+@Deprecated
 public class ExplcitCommitSQLProcessor extends SQLProcessor
 {
     public ExplcitCommitSQLProcessor(String helperName)
---
+++ entity/src/main/java/org/ofbiz/core/entity/jdbc/ExplicitCommitSQLProcessor.java
@@ -0,0 +1,22 @@
+package org.ofbiz.core.entity.jdbc;
+
+/**
+ * This version of the {@link org.ofbiz.core.entity.jdbc.SQLProcessor} that
+ * takes sets autocommit to false on its connection and then calls commit on
+ * that connection when it is closed.
+ *
+ * It will be created with {@link org.ofbiz.core.entity.jdbc.SQLProcessor.CommitMode#EXPLICIT_COMMIT}.
+ *
+ * This class is used to "clearly document" the mode the SQLProcessor is in.
+ */
+public class ExplicitCommitSQLProcessor extends SQLProcessor {
+
+    /**
+     * Constructor.
+     *
+     * @param helperName the datasource helper name
+     */
+    public ExplicitCommitSQLProcessor(String helperName) {
+        super(helperName, CommitMode.EXPLICIT_COMMIT);
+    }
+}
