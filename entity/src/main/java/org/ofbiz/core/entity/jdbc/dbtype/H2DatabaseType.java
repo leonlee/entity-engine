2ea69f37745b7f3fd21fa5702f3621f77e4aa4af
Alexander Courtis <acourtis@atlassian.com>
2014-09-18 19:13:53 +1000

    add H2DatabaseType for convenience

---
+++ entity/src/main/java/org/ofbiz/core/entity/jdbc/dbtype/H2DatabaseType.java
@@ -0,0 +1,9 @@
+package org.ofbiz.core.entity.jdbc.dbtype;
+
+public class H2DatabaseType extends SimpleDatabaseType
+{
+    public H2DatabaseType()
+    {
+        super("H2", "h2", new String[]{"H2 Database Engine"});
+    }
+}
