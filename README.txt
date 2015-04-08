f1b392d6cc489e05dac12758341f8f7b19dd891c
Chris Fuller <cfuller@atlassian.com>
2015-04-08 12:58:47 +1000

    wip

---
+++ README.txt
@@ -0,0 +1,7 @@
+
+This branch is nothing like the others.  It is meant to hold the plan templates and nothing else.
+
+It was too late to start it from an uninitialized repository, so it's built off of the first commit
+with all files deleted instead.  This branch should never be merged into any other.
+
+
---
+++ conf/entity-engine.groovy
@@ -0,0 +1,54 @@
+// dependent on templates/shortcuts/defaultBuilds.groovy
+
+class EntityEngine
+{
+    // project specific settings
+    static owner = 'cfuller'
+    static key = 'OFBEE'
+    static name = 'OfBiz Entity Engine Fork'
+    static repository = 'entity-engine'
+
+    static includeDefaults(configuration)
+    {
+        // Insert defaults into configuration
+        [
+                owner : owner,
+                projectKey : key,
+                projectName : name,
+                planDescription: "${name} - ${configuration.planName}",
+                sharedRepoName : repository,
+                enableJdk16BuildStage : 'true',
+                enableJdk17BuildStage : 'true',
+                enableJdk18BuildStage : 'true',
+                enableReleaseStage : 'true',
+                jobType : 'maven3',
+                jobMavenExe : 'Maven 3.2',
+                jobGoal : 'clean verify',
+                label : 'platform',
+                releaseJavaVersion : 'JDK 1.6',
+                hasTests : 'true',
+        ].each { key, value -> configuration.get(key, value) }
+        // Stringify everything in to evaluate GString to String
+        configuration.each { key, value -> configuration[key] = value.toString() }
+    }
+}
+
+templateRepository(url: "https://bitbucket.org/atlassian/java-platform-plan-templates")
+
+// maven 3
+[ '1.0' ].each
+        { projectBranch ->
+            defaultPlan(EntityEngine.includeDefaults(
+                    planKey : "${EntityEngine.key}B${projectBranch.replace('.','D')}X",
+                    planName : "${projectBranch}.x",
+                    branch : "${projectBranch}.x",
+                    issueSuffix : "-${projectBranch}",
+            ))
+        }
+
+defaultPlan(EntityEngine.includeDefaults(
+        planKey : "${EntityEngine.key}M",
+        planName : 'master',
+        branch : 'master',
+        issueSuffix : '',
+))
